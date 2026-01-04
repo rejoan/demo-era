66e5677e80076f289892e2b95d88d5fb0ec611d0
Rejoan <rejoan.er@gmail.com>
2026-01-04 13:24:59 +0600

    reduce padding

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,15 +2,15 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-const Navbar = ({ href }) => {
+const Navbar = () => {
   const pathname = usePathname();
   return (
     <div className="col-span-2 bg-green-900 h-screen">
       <Link href="/"><p className="text-center text-white ps-5 pt-3"></p></Link>
-      <Link href="/application"><p className={`text-white hover:bg-green-800 p-5 ${pathname === '/application' ? 'bg-green-600' : ''}`}>My Application</p></Link>
-      <Link href="/application/new"><p className={`text-white hover:bg-green-800 p-5 ${pathname === '/application/new' ? 'bg-green-600' : ''}`}>Create</p></Link>
-      <Link href="/notification"><p className={`text-white hover:bg-green-800 p-5 ${pathname === '/notification' ? 'bg-green-600' : ''}`}>Notifications</p></Link>
-      <Link href="/logout"><p className="text-white hover:bg-red-500 p-5">Sign Out</p></Link>
+      <Link href="/application"><p className={`text-white hover:bg-green-800 p-3 ${pathname === '/application' ? 'bg-green-600' : ''}`}>My Application</p></Link>
+      <Link href="/application/new"><p className={`text-white hover:bg-green-800 p-3 ${pathname === '/application/new' ? 'bg-green-600' : ''}`}>Create</p></Link>
+      <Link href="/notification"><p className={`text-white hover:bg-green-800 p-3 ${pathname === '/notification' ? 'bg-green-600' : ''}`}>Notifications</p></Link>
+      <Link href="/logout"><p className="text-white hover:bg-red-500 p-3">Sign Out</p></Link>
     </div>
   );
 };
